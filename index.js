099db8c5477d3631f8768a16431d86cfc136a812
Hector-pellegrino <102663611+Hector-pellegrino@users.noreply.github.com>
2023-11-01 12:28:47 -0300

    recebendo informações do formulário no corpo da requisição

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require ("express")
 const path = require("path")
 
-
-const caminhoBase = path.join(__dirname, "templates")
 const app = express()
+const caminhoBase = path.join(__dirname, "templates")
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+
+app.post ('/cadastrar/salvar', (require, resposta) => {
+    const nome = require.body.nome
+    const email = require.body.email
+    const senha = require.body.senha
+    console.log(`O nome do usuário é ${nome}`)
+})
+
+app.get('/cadastrar', (require, resposta) => {
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
+})
 
 app.get('/usuarios/:id', (require, resposta) => {
     const id = require.params.id
